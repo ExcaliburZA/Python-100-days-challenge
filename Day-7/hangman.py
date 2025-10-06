8ff04fddfd2b2e26a053e27ac50379d84a31e614
Sam Dudley <samuel.dudley@rank.com>
2025-10-06 14:39:03 +0200

    Hangman WIP attempt 1.2 - step 2 logic fully working!

--- Day-7/hangman.py
+++ Day-7/hangman.py
@@ -1,4 +1,5 @@
 import random
+
 word_list = ["aardvarkk", "baboonn", "camell"]
 
 chosen_word = random.choice(word_list)
@@ -6,7 +7,6 @@ print(chosen_word)
 chosen_word_chars = list(chosen_word)
 
 # TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
-
 
 answer = []
 guess = ""
@@ -21,14 +21,23 @@ while "".join(answer) != chosen_word:
   guess = input("Guess a letter: ").lower()
   iteration += 1
 
+  if not guess in list(chosen_word):
+    print("Incorrect!")
+    continue
+
   for letter in chosen_word_chars:
-    while not letterFound:
+    while True:
       try:
         guessIndex = chosen_word_chars.index(guess)
         print(guess + " found at index " + str(guessIndex))
         answer[guessIndex] = guess.upper()
         chosen_word_chars[guessIndex] = ""
+
       except ValueError:
         break
   print(f"Answer: {"".join(answer)}")
+
+# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+
+
 
