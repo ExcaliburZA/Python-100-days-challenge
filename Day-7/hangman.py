194b5ee6ef03db887113c997eec771def21ebbaf
Sam Dudley <samuel.dudley@rank.com>
2025-10-06 14:05:55 +0200

    Hangman WIP attempt 1.1 - mostly working

--- Day-7/hangman.py
+++ Day-7/hangman.py
@@ -5,10 +5,12 @@ chosen_word = random.choice(word_list)
 print(chosen_word)
 chosen_word_chars = list(chosen_word)
 
+# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
+
+
 answer = []
 guess = ""
 guessIndex = 0
-letterFound = False
 iteration = 0
 
 for letter in chosen_word_chars:
@@ -26,13 +28,7 @@ while "".join(answer) != chosen_word:
         print(guess + " found at index " + str(guessIndex))
         answer[guessIndex] = guess.upper()
         chosen_word_chars[guessIndex] = ""
-        letterFound = True
       except ValueError:
         break
   print(f"Answer: {"".join(answer)}")
-
-#Attempt 1 - works for 1 instance of letter per word
-#______________________________________________________________________
-
-
 
